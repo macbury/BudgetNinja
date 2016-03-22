177b8fc219fa32d3f02c3610b7917820f31eaaaf
Arkadiusz Buras <macbury@gmail.com>
2016-03-22 07:39:38 +0000

    Install node js in travis ci

--- spec/support/webpack.rb
+++ spec/support/webpack.rb
@@ -2,7 +2,7 @@
 module WebpackSupport
 
   def self.start!
-    @thread = Thread.new { @pid = spawn("cd #{Rails.root} && foreman start") } # run webpack server in subprocess
+    @thread = Thread.new { @pid = spawn("cd #{Rails.root} && bundle exec foreman start") } # run webpack server in subprocess
     sleep 1 # wait just one second to allow everything to boot up
   end
 
