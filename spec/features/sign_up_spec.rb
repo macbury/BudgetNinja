9bda5d53a80e9d14b22e388b00f4f8d71d0daf15
Arkadiusz Buras <macbury@gmail.com>
2016-03-29 15:58:16 +0000

    Additional errors for testing validations on signup

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -25,6 +25,20 @@ feature 'User sign up', js: true do
       expect(page).to have_content(success_sign_up_flash_message)
       expect(User.count).to eq(1)
     end
+
+    scenario 'i should get errors after entering wrong data into fields' do
+      user = create(:user)
+      visit auth_path
+
+      fill_in email_label, with: user.email
+      fill_in password_confirmation_label, with: user.password
+      fill_in password_label, with: 'test'
+
+      click_on sign_up_button
+
+      expect(page).to have_content('already in use')
+      expect(page).to have_content('is too short (minimum is 8 characters)')
+    end
   end
 
   context 'as logged in user' do
